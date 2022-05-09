89a07230e522aa6a377c61fb9115d2803b40b57f
Shaowei Lin <shaoweilin.research@gmail.com>
2022-05-08 20:37:10 -0700

    html_extra_path

--- conf.py
+++ conf.py
@@ -64,7 +64,7 @@ html_favicon = "_static/favicon.ico"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_extra_path = ["feed.xml"]
+html_extra_path = ["feed.xml","extra"]
 html_sidebars = {
     "index": ["hello.html"],
     "about": ["hello.html"],
